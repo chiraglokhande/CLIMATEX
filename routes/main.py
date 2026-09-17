035350cc6c6bbee6396e310032ab2a22dcba0c09
chiraglokhande <chiraglokhande@users.noreply.github.com>
2026-09-17 23:50:50 +0530

    feat: make CLIMATEX fully responsive for mobile, tablet, and desktop devices

--- routes/main.py
+++ routes/main.py
@@ -1,16 +1,9 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    if request.args.get('debug'):
-        return jsonify({
-            'request.path': request.path,
-            'request.url': request.url,
-            'headers': dict(request.headers),
-            'environ': {k: str(v) for k, v in request.environ.items() if isinstance(v, (str, int, float, bool))}
-        })
     return render_template('index.html')
 
 @main_bp.route('/about')
